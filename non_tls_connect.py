e8d9e610d295dcc4db395c83677b2398c5ed4449
Amp <amp@amperture.com>
2016-11-20 23:38:17 +0000

    Basic adminstatus check implemented.

--- non_tls_connect.py
+++ non_tls_connect.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import pydle
 
 # name, irc server, and channel for the bot to join. also setup the list of admin users that can control the bot
 
-botname = 'aiurbot'
+botname = 'ampbot'
 server = 'irc.quakenet.org'
 channel = '#aiur'
-admin_nicks = [ 'booboy', 'booboywork', 'Amp', 'AmpWork' ]
+adminNick = [ 'booboy', 'booboywork', 'Amp', 'AmpWork']
 
 class aiurBot(pydle.Client):
     """ behold the aiur irc bot! """
@@ -20,6 +20,36 @@ class aiurBot(pydle.Client):
     def on_join(self, channel, user):
         super().on_join(channel, user)
         self.message(channel,'Greetings, I am ' + botname)  
+
+    @pydle.coroutine
+    def is_admin(self, nickname):
+        """
+        Check whether or not a user has admin priviledge.
+        """
+        if nickname in adminNick:
+            return True
+            #info = yield self.whois(source)
+            #admin = info['identified']
+        return False
+
+    @pydle.coroutine
+    def on_message(self, target, source, message):
+        super().on_message(target, source, message)
+
+        # Tell a user if they are an administrator for this bot.
+        if message.startswith('!adminstatus'):
+            admin = yield self.is_admin(source)
+
+        if admin == True:
+            print("User "+source+" is an admin!")
+            self.message(channel, 
+                    source+': You are an administrator.'
+            )
+        else:
+            print("User "+source+" is not an admin!")
+            self.message(channel, 
+                    source+': You are not an administrator.', 
+            )
 
 client = aiurBot(botname)
 client.connect(server, tls=False)
