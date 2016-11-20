bc7f1b1d5856d2ef7851263bcfe5920df3932ee7
Brendan McDevitt <bpmcdevitt@thelinuxspace.com>
2016-11-20 15:47:24 -0600

    robot joins channel and sends greetings msg to chan

--- non_tls_connect.py
+++ non_tls_connect.py
@@ -2,18 +2,26 @@
 
 import pydle
 
+# name, irc server, and channel for the bot to join. also setup the list of admin users that can control the bot
+
+botname = 'aiurbot'
+server = 'irc.quakenet.org'
+channel = '#aiur'
+admin_nicks = [ 'booboy', 'booboywork', 'Amp', 'AmpWork' ]
+
 class aiurBot(pydle.Client):
-    """ This is a simple bot that will greet people as they join the channel. """
+    """ behold the aiur irc bot! """
 
     def on_connect(self):
         super().on_connect()
         # Can't greet many people without joining a channel.
-        self.join('#aiur')
+        self.join(channel)
 
     def on_join(self, channel, user):
         super().on_join(channel, user)
-        self.message(channel, 'Hey there, {user}!', user=booboywork)
+        self.message(channel,'Greetings, I am ' + botname)  
 
-client = aiurBot('aiurbot')
-client.connect('irc.quakenet.org', tls=False)
+client = aiurBot(botname)
+client.connect(server, tls=False)
 client.handle_forever()
+
